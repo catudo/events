7ab6903d2faa8ddf6938d4a48a77eaf758ff0e51
Carlos Gutierrez <carguterrez@100grados.co>
2020-01-25 16:03:48 -0500

    first test

---
+++ main.js
@@ -0,0 +1,30 @@
+// REQUIRE ALL THE LIBRARIES
+// =============================================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+app.listen(8080,'0.0.0.0',  function () {
+    console.log('events application listen at 8080');
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
