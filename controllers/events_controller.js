1cbb0c91fc7e6b90d6977c92195305c1a79697a1
Carlos Gutierrez <carguterrez@100grados.co>
2020-01-25 19:54:59 -0500

    login done

---
+++ controllers/events_controller.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+module.exports = function (dbs) {
+    var Event = require('../models/event')(dbs);
+    //POST - /event/save_event
+    var save_event = function(req, res, next) {
+        var email = req.body.email;
+        var promise = User.findOne({email: email}).exec();
+        promise.then(function (user) {
+            return user;
+        }).then(function (user) {
+            var newUser = new User(req.body);
+            newUser.save().then(function (user_saved) {
+                return next({saved:true});
+            }).catch(function (err) {
+                if (err.errorCode === 422) {
+                    var responseError = {};
+                    responseError.statusCode = err.errorCode;
+                    responseError.validationErrors = [{
+                        keyword: err.name,
+                        message: err.message
+                    }];
+                    res.status(422).json(responseError)
+                } else {
+                    return next(err);
+                }
+            });
+        })
+    }
+
+    router.post('/users/signup',  signup);
+
+    return router;
+
+}
