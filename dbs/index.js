3afd7e48243613a962f08b6e470b5bbea6bb887f
Carlos Gutierrez <carguterrez@100grados.co>
2020-01-27 18:09:27 -0500

    add user permision

--- dbs/index.js
+++ dbs/index.js
@@ -13,7 +13,7 @@ const options = {
 };
 
 module.exports = {
-    dbMaster: mongoose.createConnection('mongodb://127.0.0.1:27017/events', options, function (err, res) {
+    dbMaster: mongoose.createConnection('mongodb://catudo:3p51l0n2018*@127.0.0.1:27017/events', options, function (err, res) {
         if (err) {
             console.log('Error: connecting to Database ');
             process.exit(1);
